bea7dc2686737254446c6291eb79d4e3a76e8f97
baoanh1310 <dataholicguy@gmail.com>
2020-05-02 23:38:42 +0700

    Update ImageTransform class

--- image_transform.py
+++ image_transform.py
@@ -2,19 +2,19 @@ from lib import *
 from config import *
 
 class ImageTransform():
-    def __init__(self, resize, mean):
+    def __init__(self, resize, mean, std):
         self.data_transform = {
             'train': transforms.Compose([
                 transforms.RandomResizedCrop(resize, scale=(0.5, 1.0)),
-                transforms.RandomRotation(40)
-                transforms.RandomAffine(translate=(0.2, 0.2), shear=0.2),
+                transforms.RandomRotation(40),
+                transforms.RandomAffine(degrees=0, translate=(0.2, 0.2), shear=0.2),
                 transforms.RandomHorizontalFlip(1.0),
                 transforms.ToTensor(),
-                transforms.Normalize(mean)
+                transforms.Normalize(mean, std)
                 ]),
             'val': transforms.Compose([
                 transforms.ToTensor(),
-                transforms.Normalize(mean)
+                transforms.Normalize(mean, std)
                 ]),
             'test': transforms.Compose([
                 transforms.Resize(resize),
